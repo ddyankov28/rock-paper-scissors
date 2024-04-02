31e450560660939a96dfca7262fcfea417c1d8b7
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-04-02 15:53:59 +0200

    user interface

--- rps.js
+++ rps.js
@@ -1,26 +1,21 @@
-function getComputerChoice()
-{
-    return Math.floor(Math.random() * 3)
-}
-
 function playRound(playerSelection, computerSelection)
 {
     if ((playerSelection === "rock" && computerSelection === "rock")
         || (playerSelection === "paper" && computerSelection === "paper")
         || (playerSelection === "scissors" && computerSelection === "scissors"))
-        return "draw";
+        return "drew";
     else if ((playerSelection === "rock" && computerSelection === "scissors")
         || (playerSelection === "paper" && computerSelection === "rock")
         || (playerSelection === "scissors" && computerSelection === "paper"))
-        return "win";
+        return "won";
     else if ((playerSelection === "rock" && computerSelection === "paper")
         || (playerSelection === "paper" && computerSelection === "scissors")
         || (playerSelection === "scissors" && computerSelection === "rock"))
-        return "lose";
+        return "lost";
 }
 
-function convertToString(){
-    let num = getComputerChoice()
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3)
     if (num === 0)
         return "rock"
     else if (num === 1)
@@ -29,32 +24,51 @@ function convertToString(){
         return "scissors"
 }
 
+
+
+
 function playGame(){
-    let won = 0
-    let draw = 0
-    let lost = 0
-    
-    for (let i = 0; i < 5; i++)
-    {
-        let userInput = prompt("Rock, Paper or Scissors\n>>> 5 Rounds <<<\nEnter your choice")
-        let playerSelection = userInput.toLowerCase()
-        if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
-        {
-            console.log("Wrong input, Try again")
-            i -= 1
-            continue        
-        }
-        let computerSelection = convertToString()
-        let roundResult = playRound(playerSelection, computerSelection)
-        if (roundResult === "win")
-            won++
-        else if (roundResult === "lose")
-            lost++
-        else
-            draw++
-        console.log(roundResult)
-    }
-    console.log("You won " + won + ", drew " + draw + " and lost " + lost + " rounds in this game")
+
+    let roundScore = 0;
+
+    rockButton.addEventListener("click", () =>{ 
+        roundMessage.textContent = "This round you "; 
+        roundScore = playRound("rock", getComputerChoice());
+        roundMessage.textContent += roundScore;
+    });
+    paperButton.addEventListener("click", () => {
+        roundMessage.textContent = "This round you ";
+        roundScore = playRound("paper", getComputerChoice());
+        roundMessage.textContent += roundScore;
+    });
+    scissorsButton.addEventListener("click", () => {
+        roundMessage.textContent = "This round you ";
+        roundScore = playRound("scissors", getComputerChoice());
+        roundMessage.textContent += roundScore;
+    });
 }
+
+let won = 0
+let draw = 0
+let lost = 0
+
+const container = document.querySelector("#container");
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorsButton = document.createElement("button");
+const roundMessage = document.createElement("h1");
+const myScore = document.createElement("h1");
+const computerScore = document.createElement("h1");
+
+rockButton.textContent = "Rock";
+paperButton.textContent = "Paper";
+scissorsButton.textContent = "Scissors";
+
+container.appendChild(rockButton);
+container.appendChild(paperButton); 
+container.appendChild(scissorsButton);
+
+roundMessage.textContent = "This round you ";
+container.appendChild(roundMessage);
 
 playGame()
